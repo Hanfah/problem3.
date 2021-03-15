a83da271c4738e43eece2cebf7f89e901984ffc1
Hanfah <hanfah@mail.com>
2021-03-15 13:08:36 -0900

    code updated

--- problem3.js
+++ problem3.js
@@ -1,11 +1,17 @@
-var myString = "how many times does t come in this sentence?"
-count = 0;
+//write a javascript function that accepts two arguments , a string and a letter and the function will count the number of
+//occurences of the specified letter with in the str
 
-for(var i = 0; i < myString.length; i++) {
-
-	if(myString.charAt(i) === 't') {
-		count++;
+var str="function to get the number of occurences of a given letter";
+function char_count(){
+	//document.writeln(str);
+	var letter = "e";
+	var letter_Count =0;
+	for(var position=0;position<str.length;position++){
+		if(str.charAt(position)==letter){
+			letter_Count +=1;
+		}
 	}
+	return "<br> Occurence of " +letter + " is= "+ letter_Count;
 }
 
-console.log(count)
+console.log(char_count());
