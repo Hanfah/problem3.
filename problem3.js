b2362d0335c157232446ff1fa0d461ec920a197c
Hanfah <hanfah@mail.com>
2021-03-18 09:52:37 -0900

    files updated

--- problem3.js
+++ problem3.js
@@ -1,10 +1,11 @@
 //write a javascript function that accepts two arguments , a string and a letter and the function will count the number of
 //occurences of the specified letter with in the str
 
-var str="function to get the number of occurences of a given letter";
-function char_count(){
+var stringPassedIn="function to get the number of occurences of a given letter";
+var keyletter="o";
+function char_count(str, letter){
 	//document.writeln(str);
-	var letter = "e";
+
 	var letter_Count =0;
 	for(var position=0;position<str.length;position++){
 		if(str.charAt(position)==letter){
@@ -14,4 +15,4 @@ function char_count(){
 	return "<br> Occurence of " +letter + " is= "+ letter_Count;
 }
 
-console.log(char_count());
+console.log(char_count(stringPassedIn,keyletter));
